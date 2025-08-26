5e164391dc522714829f28b055461b7729227a7e
Safintc <safintc7899@gmail.com>
2025-08-26 18:30:08 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -31,7 +31,8 @@ async def filter(client: Bot, message: Message):
      if re.findall(r"^(\/|,|!|\.|[\U0001F600-\U0001F64F])", message.text):
         return
 
-    if 2 < len(message.text) < 50:    
+   if message.text and 2 < len(message.text) < 50:
+      
         btn = []
 
         group_id = message.chat.id
